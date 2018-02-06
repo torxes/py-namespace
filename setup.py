2eaf9c0020ea97cef3ae7f8b9d538386f74e1ac5
Urs Klingsporn <urs.klingsporn@googlemail.com>
2018-02-06 19:33:10 +0100

    Enable Travis-CI integration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="pyns",
-        version="0.0.1.dev",
+        version="0.0.1.dev1",
         url="https://github.com/torxes/py-namespace",
         author="Urs Klingsporn",
         author_email="urs.klingsporn@googlemail.com",
@@ -11,8 +11,13 @@ if __name__ == '__main__':
         packages=['pyns'],
         zip_safe=False,
         install_requires=[
-            'pytest>=3.4.0',
             'six>=1.11.0',
             'PyYAML>=3.12'
+        ],
+        tests_require=[
+            'pytest>=3.4.0',
+        ],
+        setup_requires=[
+            'pytest-runner>=3.0',
         ]
     )
